bedd942a7a7737bc2cee44178075fe6f989983fc
captainseesaw <lizhaoshuo12345@126.com>
2017-11-20 11:21:14 -0800

    updated to python 3 and qt5

--- mainWindow.py
+++ mainWindow.py
@@ -3,12 +3,12 @@
 
 import os
 import sys
-from PyQt4.QtGui import *
-from PyQt4 import uic
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 import serial
-import serial.tools.list_ports
 import struct
 
 
@@ -21,16 +21,16 @@ class MainWindow(QMainWindow):
 		self.show()
 		
 		# connect button signal
-		QObject.connect(self.btnConnect, SIGNAL('clicked()'), self.btnConnect_clicked)
-		QObject.connect(self.btnStart, SIGNAL('clicked()'), self.btnStart_clicked)
+		self.btnConnect.clicked.connect(self.btnConnect_clicked)
+		self.btnStart.clicked.connect(self.btnStart_clicked)
 
 		# setup timer
 		self.timer = QTimer(self)
-		QObject.connect(self.timer, SIGNAL('timeout()'), self.timer_timeout)
+		# self.timer.timeout.connect(self.timer_timeout)
 		self.timer.start(1000)
 
 	def btnStart_clicked(self):
-		print "start clicked"
+		print ("start clicked")
 		sevoLeft = 1806
 		servoRight = 1053
 		# Create four bytes from the integer
@@ -44,17 +44,17 @@ class MainWindow(QMainWindow):
 	def btnConnect_clicked(self):
 		try:
 			self.ser = serial.Serial('COM14',9600,timeout = 1)
-			print "connection established"
+			print ("connection established")
 		except serial.serialutil.SerialException:
-			print "serial port not available"
+			print ("serial port not available")
 		# print(ser.name)
 		# ser.write(self.txtServoLeft,self.txtServoRight)
 		# ser.close()
 		
-	def timer_timeout(self):
+	#def timer_timeout(self):
 		# self.ser.write(1234)
 		# print self.ser.read()
-		portInfo = serial.tools.list_ports.comports()
+		# portInfo = serial.tools.list_ports.comports()
 		# for i in range (0,len(portInfo)):
 			# print portInfo[i].name
 
